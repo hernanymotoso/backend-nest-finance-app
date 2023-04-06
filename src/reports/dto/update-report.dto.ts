658d290a058ef826a038c3e1fee24863f75e894b
Hernany Motoso <contatohernany@gmail.com>
2023-04-06 16:26:16 -0300

    feat: build services and dto

--- src/reports/dto/update-report.dto.ts
+++ src/reports/dto/update-report.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReportDto } from './create-report.dto';
+import { ReportStatus, ReportStatusList } from '../entities/report.entity';
+import { IsIn, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
-export class UpdateReportDto extends PartialType(CreateReportDto) {}
+export class UpdateReportDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  file_url: string;
+
+  @IsIn(ReportStatusList)
+  @IsNotEmpty()
+  status: ReportStatus;
+}
