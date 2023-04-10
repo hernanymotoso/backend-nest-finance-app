8ad300c58c50d422dbc97b9bde3c96c80f90a976
Hernany Motoso <contatohernany@gmail.com>
2023-04-09 21:16:49 -0300

    chore: enable cors and change app port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       errorHttpStatusCode: 422,
     }),
   );
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
